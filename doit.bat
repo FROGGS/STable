97760bb5b12ab351142da31429bb6f37d659bde5
Tobias Leich <email@froggs.de>
2014-02-15 16:54:00 +0100

    we dont even have to precompile Panda.pm

--- doit.bat
+++ doit.bat
@@ -1,3 +1,3 @@
 call perl6-m -I. --target=mbc --output=Panda\Common.moarvm Panda\Common.pm
 call perl6-m -I. --target=mbc --output=Panda\Project.moarvm Panda\Project.pm
-call perl6-m -I. --target=mbc --output=Panda.moarvm Panda.pm
+call perl6-m -I. -e "use Panda::Common; use Panda::Project"
--- doit.sh
+++ doit.sh
@@ -1,3 +1,3 @@
 perl6-m -I. --target=mbc --output=Panda/Common.moarvm Panda/Common.pm
 perl6-m -I. --target=mbc --output=Panda/Project.moarvm Panda/Project.pm
-perl6-m -I. --target=mbc --output=Panda.moarvm Panda.pm
+perl6-m -I. -e "use Panda::Common; use Panda::Project"
